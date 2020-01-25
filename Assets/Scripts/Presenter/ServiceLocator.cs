736fe35aeafc679d06d20cc2839e0d96073aa6b0
unknown <daniel.kamali@yahoo.com>
2020-01-25 13:29:40 +0330

    Add Pure Files

---
+++ Assets/Scripts/Presenter/ServiceLocator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceLocator : MonoBehaviour
+{
+    public EventManager eventManager;
+    public Ad adManager;
+    private static ServiceLocator instance;
+    public static ServiceLocator Instance;
+
+    private void Awake()
+    {
+        instance = this;
+        
+        if (Instance == null)
+            Instance = instance;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        SetupInterfaces();
+    }
+
+    private void SetupInterfaces()
+    {
+        eventManager = new BasicEventManager();
+        adManager = new GoogleAd();
+    }
+}
